3562243b10f6f31fe3fc2b930f0995948cbf632c
patrick dang <pdang289@mtroyal.ca>
2025-11-20 19:08:03 +0000

    deleted and renamed some file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <cstring>
+#include <iostream>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main()
+{
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    
+    
+
+    return 0;
+}
