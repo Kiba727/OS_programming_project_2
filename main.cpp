1672d7cc92eb533ef08be9c144b0f44b6e1509d0
patrick dang <pdang289@mtroyal.ca>
2025-12-06 20:36:51 +0000

    ayy multithreaded

--- main.cpp
+++ main.cpp
@@ -4,6 +4,21 @@
 #include <arpa/inet.h>
 #include <iostream>
 #include <unistd.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+sem_t thread_limiter;
+
+void* client_worker(void* arg) {
+    int client_fd = (int)(intptr_t)arg;
+
+    handle_client(client_fd);
+    
+    close(client_fd);
+    printf("[-] Client disconnected (FD: %d)\n", client_fd);
+    sem_post(&thread_limiter);
+    return NULL;
+}
 
 int main() { 
     int port = 8080;
@@ -12,24 +27,34 @@ int main() {
     // Puts it into "Listening Mode"
     int listen_fd = create_listen_socket(port);
 
-    if (listen_fd < 0) {
-        std::cerr << "Failed to open socket\n";
-        return 1;
-    }
+    sem_init(&thread_limiter, 0, 5);
+
+    printf("Server listening on port %d...\n", port);
 
     // This is the "Accept" Loop
-    while (true) {
-        sockaddr_in client{};
-        int client_fd = accept_client(listen_fd, client);
+    while (1) {
+            sem_wait(&thread_limiter);
 
-        if (client_fd < 0) continue;
+            sockaddr_in client;
+            int client_fd = accept_client(listen_fd, client);
 
-        std::cout << "[+] Client connected\n";
-        handle_client(client_fd);
-        close(client_fd);
-        std::cout << "[-] Client disconnected\n";
+            if (client_fd < 0) {
+                sem_post(&thread_limiter);
+                continue;
+            }
+
+            pthread_t thread_id;
+            
+            if (pthread_create(&thread_id, NULL, client_worker, (void*)(intptr_t)client_fd) != 0) {
+                perror("Thread creation failed");
+                close(client_fd);
+                sem_post(&thread_limiter);
+            } else {
+                pthread_detach(thread_id);
+            }
+        }
+
+        sem_destroy(&thread_limiter);
+        close(listen_fd);
+        return 0;
     }
-
-    close(listen_fd);
-    return 0;
-}
