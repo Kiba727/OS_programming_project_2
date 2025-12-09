82a528763374ee7ebcef233e8997720ec2b2b1c1
patrick dang <pdang289@mtroyal.ca>
2025-12-09 04:32:09 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,23 @@
+How to run:
+
+Main programn
+
+1. Run make to create the following executables
+
+./server_single
+./server_multi
+./serer_pool
+
+2. run any of the above excecutables and type the following url in your browser
+
+URL: http://localhost:8080/
+
+Test programn
+
+1. Run make tests to create the following executables
+
+./test_socket
+./test_parser
+./test_thread_pool
+
+2. run any of the excecutables to test programn functions
--- socket.h
+++ socket.h
@@ -7,15 +7,14 @@
 
 /**
  * @brief Creates and configures a listening TCP socket.
+ * 
+ * This function creates a socket in the following steps
+ * creates a file descriptor
+ * binds socket with port 8080
+ * marks socket to be ready to accept connections
  *
- * This function performs the standard sequence of socket setup:
- * 1. socket(): Creates the file descriptor.
- * 2. setsockopt(): Sets SO_REUSEADDR to allow immediate restart of the server.
- * 3. bind(): Associates the socket with the specified port on all interfaces.
- * 4. listen(): Marks the socket as passive, ready to accept connections.
- *
- * @param port The port number to listen on (e.g., 8080).
- * @param backlog The maximum length of the queue of pending connections.
+ * @param port The port number to listen on 
+ * @param backlog The maximum length of the queue connections
  * @return The file descriptor of the listening socket, or -1 on error.
  */
 int create_listen_socket(int port, int backlog = 10);
@@ -23,9 +22,8 @@ int create_listen_socket(int port, int backlog = 10);
 /**
  * @brief Accepts a new incoming client connection.
  *
- * This function blocks (waits) until a client connects to the server.
- * When a connection is established, it returns a new file descriptor
- * specific to that client session.
+ * Blocks and waits until a client connects.
+ * When a connection is made returns a new file descriptor
  *
  * @param listen_fd The listening socket file descriptor created by create_listen_socket().
  * @param client_addr Reference to a sockaddr_in struct to store the client's address details.
